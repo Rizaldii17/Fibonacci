59888ae71a4b2f9e8c6347b93b32369fea9c768c
Rizaldi rachmansyah <rizaldirachmansyah17@gmail.com>
2016-03-31 10:42:01 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-   int a=0,b=0,hasil=1,c;
+   int a=0,b=0,hasil=1,c,hasiljumlah=0,y=0;
+   int jumlah[] = new int[10];
    Scanner input = new Scanner (System.in);
    System.out.print ("Masukan Deret Fibonacci  : ");
         c = input.nextInt ();
         for(int i=1; i<=c; i++){           
     a=b;
     b=hasil;
-    System.out.print(hasil+" ");
+    jumlah[i] = hasil;
     hasil=a+b;
+    y++;
     }
-  } 
+    for(int i=y; i>0; i--){
+        System.out.print(jumlah[i]+" ");
+      } 
+    }
 }
